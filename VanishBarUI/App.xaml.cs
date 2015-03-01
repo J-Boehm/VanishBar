1e03652efc5803e1a937a185114690729e9a6b75
J-Boehm <j.boehm@outlook.de>
2015-03-01 16:43:57 +0100

    Version 2.1
    * Console interface removed
    * Easier usage

--- VanishBarUI/App.xaml.cs
+++ VanishBarUI/App.xaml.cs
@@ -5,10 +5,24 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using VanDLL;
 
 namespace VanishBarUI
 {
     public partial class App : Application
     {
+        void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            if (e.Args.Length > 0)
+            {
+                if (e.Args[0] == "-h")
+                    VanishCommands.HideTaskbar();
+                else if (e.Args[0] == "-s")
+                    VanishCommands.ShowTaskbar();
+                Application.Current.Shutdown();
+            }
+            else
+                new MainWindow().Show();
+        }
     }
 }
